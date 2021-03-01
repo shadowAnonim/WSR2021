3d33139ccb00c884bc1733a60d04250538d657f4
shadowAnonim <59304755+shadowAnonim@users.noreply.github.com>
2021-03-01 20:24:36 +0300

    Добавьте файлы проекта.

---
+++ WSR2021/Pages/AutorisationPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WSR2021.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AutorisationPage.xaml
+    /// </summary>
+    public partial class AutorisationPage : Page
+    {
+        public AutorisationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void passwordCb_Checked(object sender, RoutedEventArgs e)
+        {
+            if (passwordPb.Visibility == Visibility.Visible)
+            {
+                passwordTb.Text = passwordPb.Password;
+                passwordPb.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void passwordCb_Unchecked(object sender, RoutedEventArgs e)
+        {
+            passwordPb.Password = passwordTb.Text;
+            passwordPb.Visibility = Visibility.Visible;
+        }
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Utils.db.User.Load();
+            passwordTb.Text = passwordPb.Password;
+            var result = Utils.db.User.FirstOrDefault(user => user.login == loginTb.Text && user.password == passwordTb.Text);
+            if (result == null)
+            {
+                MessageBox.Show("Error");
+                return;
+            }
+            NavigationService.Navigate(new MainPage());
+        }
+    }
+}
---
+++ WSR2021/Pages/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WSR2021.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Utils.db.ComboView.Load();
+            List<ComboView> list = Utils.db.ComboView.ToList();
+            nameTextBox.ItemsSource = list;
+            MessageBox.Show("ОК");
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            comboViewDataGrid.Items.Add(nameTextBox.SelectedItem);
+        }
+    }
+}
